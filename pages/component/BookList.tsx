04585d8fa02f227efb319b44a0fe08892c0c7d41
shimulsust999 <shimulsust999@gmail.com>
2023-03-25 01:30:17 +0600

    featured book and all book selection

--- pages/component/BookList.tsx
+++ pages/component/BookList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllBook } from "../redux/BookList/action";
 import { IInitialState } from "../redux/BookList/InitialState";
@@ -8,29 +8,52 @@ import Book from "./Book";
 const BookList = () => {
   const dispatch = useAppDispatch();
   const allBook: any = useAppSelector((state) => state.bookList);
-
+  const [featuredBook, setFeaturedBook] = useState(false);
+  const featured = allBook.filter(
+    (book: IInitialState) => book.featured === true
+  );
+  console.log(featured);
   useEffect(() => {
     dispatch<any>(getAllBook);
   }, [dispatch]);
 
+  const featuredHandle = () => {
+    setFeaturedBook(true);
+  };
+
+  const allBookHandle = () => {
+    setFeaturedBook(false);
+  };
   return (
     <div className="order-2 xl:-order-1">
       <div className="flex items-center justify-between mb-12">
         <h4 className="mt-2 text-xl font-bold">Book List</h4>
 
         <div className="flex items-center space-x-4">
-          <button className="filter-btn active-filter" id="lws-filterAll">
+          <button
+            className="filter-btn active-filter"
+            id="lws-filterAll"
+            onClick={allBookHandle}
+          >
             All
           </button>
-          <button className="filter-btn" id="lws-filterFeatured">
+          <button
+            className="filter-btn"
+            id="lws-filterFeatured"
+            onClick={featuredHandle}
+          >
             Featured
           </button>
         </div>
       </div>
       <div className="lws-bookContainer">
-        {allBook.map((book: IInitialState) => {
-          return <Book book={book} key={book.id} />;
-        })}
+        {!featuredBook
+          ? allBook.map((book: IInitialState) => {
+              return <Book book={book} key={book.id} />;
+            })
+          : featured.map((book: IInitialState) => {
+              return <Book book={book} key={book.id} />;
+            })}
       </div>
     </div>
   );
